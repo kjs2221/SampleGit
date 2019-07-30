f540661d3d23fe5b703e014450c43e0c7249f486
jsoo.sh2221 <jsoo.sh2221@gmail.com>
2019-07-30 10:16:56 +0900

    hello insert

--- GitTest/src/com/controller/TestServlet.java
+++ GitTest/src/com/controller/TestServlet.java
@@ -13,6 +13,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		System.out.println("TestServlet.doGet");
+		System.out.println("hello1");
 		
 	}
 
